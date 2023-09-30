984fe49e7a86005c1217519df5cbd3c16ee499ce
unknown <nguyenthuydung55555@gmail.com>
2023-09-30 23:30:08 +0700

    register, login

---
+++ basket/src/views/pages/login/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+function Login(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login
--- basket/src/views/routes/index.js
+++ basket/src/views/routes/index.js
@@ -4,9 +4,13 @@ import User from '../pages/users';
 import Waste from '../pages/waste-bins';
 import Rubbish from '../pages/rubbish';
 import React from 'react';
+import Register from '../pages/register';
+import Login from '../pages/login';
 function AllRoutes (){
     return (
         <Routes>
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
           <Route path="/" element={<Homepage/>} />
           <Route path="/rubbish" element={<Rubbish/>} />
           <Route path="/user" element={<User/>} />
